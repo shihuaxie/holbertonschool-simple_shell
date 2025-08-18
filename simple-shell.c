5042eac55ba1665382045cab5cfc8f627be6af08
Katherine Beringer <github@katbee.au>
2025-08-18 02:19:37 +0000

    boilerplate code for a simple shell

---
+++ simple-shell.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+extern char **environ;
+
+int main()
+{
+	char *line;
+	size_t buffer_size = 0;
+	int child;
+	char *argv[] = {NULL, NULL};
+
+	child = fork();
+
+	if (child == -1)
+	{
+		perror("Fork you\n");
+		exit(EXIT_FAILURE);
+	}
+	else if (child == 0)
+	{
+		printf("#cisfun$: ");
+		if (getline(&line, &buffer_size, stdin) == 1)
+		{
+			perror("getline failed\n");
+			free(line);
+			return (1);
+		}
+		line[strcspn(line, "\n")] = '\0';
+		argv[0] = line;
+		if (execve(argv[0], argv, environ) == -1)
+		{
+			perror("./shell");
+		}
+	}
+	else
+	{
+		wait(NULL);
+		return (0);
+	}
+}
