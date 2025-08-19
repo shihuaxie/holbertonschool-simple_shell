e2b138f60bb3598bc8dbb4adfdf9022302ecc9ab
ashleigh6734 <ashleigh6734@gmail.com>
2025-08-19 08:47:40 +0000

    builtins and header file

---
+++ builtins.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "shell.h"
+
+extern char **environ;
+
+/**
+ * handle_builtin - check and executes built-in commands
+ * @argv: parsed command arguments
+ * Return: -1 if shell should exit, otherwise 0
+ */
+
+int handle_builtin(char **argv)
+{
+	int i;
+
+	/* Check if command is empty*/
+	if (argv[0] == NULL)
+		return (0);
+
+	if (strcmp(argv[0], "exit") == 0)
+		return (-1); // signal to exit shell
+	
+	/* Handle env built-in */
+	if (strcmp(argv[0], "env") == 0)
+	{
+		for (i = 0; environ[i] != NULL; i++)
+			printf("%s\n", environ[i]);
+		return(1);
+	}
+
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,9 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stddef.h>
+#include <unistd.h>
+
+int handle_builtin(char **argv);
+
+#endif /* SHELL_H */
