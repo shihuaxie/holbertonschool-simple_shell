51d7cff61f278c04873b3e67121277f700f6d8d6
GraceKayem <11557@holbertonstudents.com>
2025-08-23 06:12:48 +0000

    Update files with latest changes

--- shell.h
+++ shell.h
@@ -3,10 +3,14 @@
 
 #include <stddef.h>
 
+extern char **environ;
+
 size_t count_tokens(const char *s);
 char **tokenize_line(char *line);
 void free_argv(char **argv);
 int only_spaces(char *line);
 int handle_builtin(char **argv);
+char *find_command(char *command);
+int check_file(char **av);
 
 #endif
